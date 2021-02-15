867dc0bdd7edb4daccf83cfa2bb7050f22ce87e1
abachhu <61637576+abachhu@users.noreply.github.com>
2021-02-14 20:56:09 -0800

    Update SignIn.vue

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -2,6 +2,7 @@
     <section>
         <!-- <navigation></navigation> -->
         <!-- <h5 class="center-align">Login</h5> -->
+        <!-- test -->
         <section id="firebaseui-auth-container"></section>
     </section>
 </template>
