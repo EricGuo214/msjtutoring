7a6c4767814eb22b549fae99bacf705ff12312ca
Eric Guo <eric.w.guo@gmail.com>
2021-02-14 19:33:53 -0800

    Added website skeleton

---
+++ router/index.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import SignIn from '@/components/SignIn'
+import firebase from "firebase";
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/SignIn',
+        name: 'SignIn',
+        component: SignIn,
+        meta: {
+            guest: true
+        }
+    },
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.auth)) {
+      firebase.auth().onAuthStateChanged(user => {
+        if (!user) {
+          next({
+            path: "/SignIn",
+          })
+        } else {
+          next()
+        }
+      })
+    } else {
+      next()
+    }
+  })
+  
+  export default router
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>MSJ Tutoring</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-menu offset-y v-if="user">
+        <template v-slot:activator="{ on }">
+          <v-btn color="primar" dark v-on="on">{{ user.email }}</v-btn>
+        </template>
+        <v-list>
+          <v-list-item to="/profile?">
+            <v-list-item-title>Profile</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="onSignout">
+            <v-list-item-title>Sign out</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <v-btn v-else to="/SignIn">Sign in</v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item to="/">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import firebase from "firebase";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      drawer: false,
+      group: null,
+      user: null,
+    };
+  },
+    created() {
+    firebase.auth().onAuthStateChanged((user) => {
+      this.user = user;
+    });
+  },
+  methods: {
+    onSignout(e) {
+      e.stopPropagation();
+      firebase.auth().signOut();
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
