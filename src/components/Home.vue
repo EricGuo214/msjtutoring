9e432ca7a3639c1dd8c7a22bfd445a56042ebab9
Eric Guo <eric.w.guo@gmail.com>
2021-02-15 12:10:22 -0800

    Deployed to firebase and finished Sign-In Feature

---
+++ src/components/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Hello, this is MSJ Tutoring
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase'
+import router from './router'
 
 Vue.config.productionTip = false
 
+const firebaseConfig = {
+  apiKey: "AIzaSyALaIRlOMt8cTbBrzOhrFZz5vLO8YGV9Kk",
+  authDomain: "msj-tutor.firebaseapp.com",
+  projectId: "msj-tutor",
+  storageBucket: "msj-tutor.appspot.com",
+  messagingSenderId: "910357633256",
+  appId: "1:910357633256:web:bfd756e7077398296e515d",
+  measurementId: "G-TF7RDVBFZG"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
