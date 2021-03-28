4ae135bbf3ac6b0d6eea10ec87d99735cfec493f
abachhu <61637576+abachhu@users.noreply.github.com>
2021-03-27 20:06:12 -0700

    borders

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,33 @@
 <template>
 
-<div class = "format">
+<body>
   <div>
-      <h1 class = "title"> Our Mission </h1>
-      <p> Mission Tutoring is a website where students get paired with personalized tutors. Our goal is to create an environment where students struggling in certain areas can get the best assistance available. We have bridged the gap between students looking for help and tutors looking to assist. </p>
+    <div class = "margins">
+        <h1 class = "title"> Our Mission </h1>
+        <p> Mission Tutoring is a website where students get paired with personalized tutors. Our goal is to create an environment where students struggling in certain areas can get the best assistance available. We have bridged the gap between students looking for help and tutors looking to assist. </p>
 
-     <h3 class = "people"> Rithwik Vaidun </h3>
-     Hey! I am currenlt 16 years old and a junior at Mission San Jose High. I joined MSJ tutoring because I love to spread my knowledge with other people. I didn't want there to be a barrier
-     for people like me to not to be able to help and teach people. In my spare time I love to run and play sports, especially table tennis.
+       <h3 class = "people"> Rithwik Vaidun </h3>
+       Hey! I am currenlt 16 years old and a junior at Mission San Jose High. I joined MSJ tutoring because I love to spread my knowledge with other people. I didn't want there to be a barrier
+       for people like me to not to be able to help and teach people. In my spare time I love to run and play sports, especially table tennis.
+    </div>
   </div>
-</div>
+</body>
 </template>
 
 
 <style scoped>
 
-.format{
+body{
 
-  border-style: solid;
-  border-width: medium;
-  border-color: blue;
+  text-align: center;
+  border: 30px solid #2EC486;
+  margin: 5px;
+
+}
+
+
+.margins{
+  padding: 10px;
 }
 
 .title{
@@ -31,7 +39,8 @@
 
 .people{
 
-text-align: left;
+text-align: center;
+padding: 5px;
 
 }
 
