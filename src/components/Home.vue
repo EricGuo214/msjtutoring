1589261f3f4eaffea5989b91a537a049891ee715
abachhu <61637576+abachhu@users.noreply.github.com>
2021-03-27 19:29:44 -0700

    StarterCSS

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,22 @@
 <template>
     <div>
-        <h1> Our Mission </h1>
+        <h1 class = "txts"> Our Mission </h1>
        Mission Tutoring is a website where students get paired with personalized tutors.Our goal is to create an environment where students struggling in certain areas can get the best assistance available. We have bridged the gap between students looking for help and tutors looking to assist.
 
        <h3> Rithwik Vaidun </h3>
-       Hey! I am currenlt 16 years old and a junior at Mission San Jose High. I joined MSJ tutoring because I love to spread my knowledge with other people. I didn't want there to be a barrier 
+       Hey! I am currenlt 16 years old and a junior at Mission San Jose High. I joined MSJ tutoring because I love to spread my knowledge with other people. I didn't want there to be a barrier
        for people like me to not to be able to help and teach people. In my spare time I love to run and play sports, especially table tennis.
-
     </div>
 </template>
+
+
+<style scoped>
+
+.txts{
+
+  text-align:center;
+  color: green;
+
+}
+
+</style>
