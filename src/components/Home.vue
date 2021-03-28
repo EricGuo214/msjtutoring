c913970276cf51d16e232278e4eec9853690231a
abachhu <61637576+abachhu@users.noreply.github.com>
2021-03-27 19:39:06 -0700

    updatedCSS

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,37 @@
 <template>
-    <div>
-        <h1 class = "txts"> Our Mission </h1>
-       Mission Tutoring is a website where students get paired with personalized tutors.Our goal is to create an environment where students struggling in certain areas can get the best assistance available. We have bridged the gap between students looking for help and tutors looking to assist.
 
-       <h3> Rithwik Vaidun </h3>
-       Hey! I am currenlt 16 years old and a junior at Mission San Jose High. I joined MSJ tutoring because I love to spread my knowledge with other people. I didn't want there to be a barrier
-       for people like me to not to be able to help and teach people. In my spare time I love to run and play sports, especially table tennis.
-    </div>
+<div class = "format">
+  <div>
+      <h1 class = "title"> Our Mission </h1>
+      <p> Mission Tutoring is a website where students get paired with personalized tutors. Our goal is to create an environment where students struggling in certain areas can get the best assistance available. We have bridged the gap between students looking for help and tutors looking to assist. </p>
+
+     <h3 class = "people"> Rithwik Vaidun </h3>
+     Hey! I am currenlt 16 years old and a junior at Mission San Jose High. I joined MSJ tutoring because I love to spread my knowledge with other people. I didn't want there to be a barrier
+     for people like me to not to be able to help and teach people. In my spare time I love to run and play sports, especially table tennis.
+  </div>
+</div>
 </template>
 
 
 <style scoped>
 
-.txts{
+.format{
+
+  border-style: solid;
+  border-width: medium;
+  border-color: blue;
+}
+
+.title{
 
   text-align:center;
   color: green;
+
+}
+
+.people{
+
+text-align: left;
 
 }
 
