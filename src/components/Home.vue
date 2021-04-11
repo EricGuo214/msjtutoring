b6b6eb8a7e2d45dd71c4c2ac55b47e81e0c7dba8
abachhu <61637576+abachhu@users.noreply.github.com>
2021-04-11 15:50:32 -0700

    font-changes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -25,35 +25,32 @@
         <table class = "table">
           <tr>
             <td class = "td1">
-              <img src = "./david.jpg" >
-              <h3 class = "people"> David Guo </h3>
-              <p>Nice to meet you! I created and led MSJ tutoring because I wanted to create a tool that would help struggling students and encourage kindness. With MSJ Tutoring, exceptional students are given
-                 the oppurtunity to gain experience in teaching and struggling students can learn from their successful peers. My hobbies are dancing, running, and playing the piano!</p>
-             </td>
-             <td class = "td1">
-               <img src = "./ericPic.jpeg" >
-               <h3 class = "people"> Eric Guo </h3>
-               <p>I joined MSJ tutoring to improve Mission San Jose High School's student experiences in challenging courses. My goal is to develop a website that benefits all participants with meaningful experiences and a sense of community. 
-                 I hope that everybody who uses this website achieves their desired results! I enjoy dancing, reading, and exercise. </p>
-              </td>
-          </tr>
-          <tr>
-            <td class = "td1">
               <img src = "./ankith_pic.png" >
               <h3 class = "people"> Ankith Bachhu </h3>
               <p>Hello! I am currently 16 years old and a junior at Mission San Jose High. I joined MSJ tutoring because I wanted to give people an opportunity to get help if they needed it. I wanted to spread my
-              knowledge with the community. In my spare time I love to code and run.</p>
+              knowledge with the community. I hope that anyone who uses this website will find academic success. In my spare time I love to code and run.</p>
              </td>
+
+             <td class = "td1">
+               <img src = "./david.jpg" >
+               <h3 class = "people"> David Guo </h3>
+               <p>Nice to meet you! I created and led MSJ tutoring because I wanted to create a tool that would help struggling students and encourage kindness. With MSJ Tutoring, exceptional students are given
+                  the oppurtunity to gain experience in teaching and struggling students can learn from their successful peers. My hobbies are dancing, running, and playing the piano!</p>
+              </td>
+
              <td class = "td1">
                <img src = "./RithPic.jpg" >
                <h3 class = "people"> Rithwik Vaidun </h3>
                <p>Hey! I am currently 16 years old and a junior at Mission San Jose High. I joined MSJ tutoring because I love to spread my knowledge with other people. I didn't want there to be a barrier
                for people like me to not to be able to help and teach people. In my spare time I love to run and play sports, especially table tennis. </p>
               </td>
-          </tr>
 
-          <tr>
-
+             <td class = "td1">
+               <img src = "./ericPic.jpeg" >
+               <h3 class = "people"> Eric Guo </h3>
+               <p>I joined MSJ tutoring to improve Mission San Jose High School's student experiences in challenging courses. My goal is to develop a website that benefits all participants with meaningful experiences and a sense of community.
+                 I hope that everybody who uses this website achieves their desired results! I enjoy dancing, reading, and exercise. </p>
+              </td>
           </tr>
 
         </table>
@@ -100,6 +97,8 @@ padding: 5px;
 .testing{
   padding-left: 250px;
   padding-right: 250px;
+  font-family: monospace;
+  font-size: 20px;
 }
 
 .table {
@@ -125,6 +124,7 @@ img {
   width: 400px;
   border: 7px solid white;
   padding: 15px;
+  font-family: cursive;
 }
 
 
