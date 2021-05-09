3e060b089bb559c644c0dbd7d82052014bba6c83
David Guo <david.dw.guo@gmail.com>
2021-05-08 17:52:09 -0700

    input required for tutor signup, updated signup and display

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,10 +1,18 @@
 <template>
+  <div>
+    <h1 class="text-center display-2 primary--text text-accent-3">
+      Welcome to MSJ Tutoring!
+    </h1>
+    <h3 class="text-center display-2 primary-text text-accent-3">
+      You must be signed in to use some functions.
+    </h3>
     <section>
-        <!-- <navigation></navigation> -->
-        <!-- <h5 class="center-align">Login</h5> -->
-        <!-- test -->
-        <section id="firebaseui-auth-container"></section>
+      <!-- <navigation></navigation> -->
+      <!-- <h5 class="center-align">Login</h5> -->
+      <!-- test -->
+      <section id="firebaseui-auth-container"></section>
     </section>
+  </div>
 </template>
 
 <script>
@@ -13,26 +21,26 @@ import firebase from "firebase";
 import * as firebaseui from "firebaseui";
 import "firebaseui/dist/firebaseui.css";
 export default {
-    name: "SignIn",
-    data() {
-        return {};
-    },
-    components: {
-    },
-    mounted() {
-        let ui = firebaseui.auth.AuthUI.getInstance();
-        if (!ui) {
-            ui = new firebaseui.auth.AuthUI(firebase.auth());
-        }
-        var uiConfig = {
-            signInSuccessUrl: "/", // This redirect can be achived by route using callback.
-            signInFlow: "popup",
-            signInOptions: [
-                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-                firebase.auth.EmailAuthProvider.PROVIDER_ID
-            ]
-        };
-        ui.start("#firebaseui-auth-container", uiConfig);
+  name: "SignIn",
+  data() {
+    return {};
+  },
+  components: {},
+  mounted() {
+    let ui = firebaseui.auth.AuthUI.getInstance();
+    if (!ui) {
+      ui = new firebaseui.auth.AuthUI(firebase.auth());
     }
+    var uiConfig = {
+      signInSuccessUrl: "/", // This redirect can be achived by route using callback.
+      signInFlow: "popup",
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      ],
+    };
+    ui.start("#firebaseui-auth-container", uiConfig);
+  },
 };
 </script>
